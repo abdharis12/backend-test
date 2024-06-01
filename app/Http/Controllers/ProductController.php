b668cb65614879236451535e6f7730a00ecd78b8
abdharis12 <abdharis.datuamas@gmail.com>
2024-06-01 17:43:07 +0700

    First Commit

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Product;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
@@ -22,6 +23,27 @@ class ProductController extends Controller
     public function store(Request $request)
     {
         //
+        $request->validate([
+            'nama_barang' => 'required|string|max:255',
+            'stok' => 'required|integer',
+            'jumlah_terjual' => 'required|integer',
+            'tgl_transaksi' => 'required|date_format:d/m/Y',
+            'jenis_barang' => 'required|string|max:255',
+        ]);
+
+        $tanggalTransaksi = Carbon::createFromFormat('d/m/Y', $request->get('tgl_transaksi'));
+
+        $product = new Product([
+            'nama_barang' => $request->get('nama_barang'),
+            'stok' => $request->get('stok'),
+            'jumlah_terjual' => $request->get('jumlah_terjual'),
+            'tgl_transaksi' => $tanggalTransaksi,
+            'jenis_barang' => $request->get('jenis_barang'),
+        ]);
+
+        $product->save();
+
+        return response()->json($product);
     }
 
     /**
@@ -44,8 +66,16 @@ class ProductController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Product $product)
+    public function destroy($id)
     {
         //
+        $product = Product::find($id);
+
+        if ($product) {
+            $product->delete();
+            return response()->json(['message' => 'Product deleted successfully']);
+        } else {
+            return response()->json(['message' => 'Product not found'], 404);
+        }
     }
 }
